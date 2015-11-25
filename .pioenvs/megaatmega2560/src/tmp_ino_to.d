111ef195bea6da64669a44773e7946f8ac56dd80
rcadey <roba.parkin@gmail.com>
2015-11-25 13:59:59 +0000

    first commit of slider arduino

---
+++ .pioenvs/megaatmega2560/src/tmp_ino_to.d
@@ -0,0 +1,16 @@
+.pioenvs/megaatmega2560/src/tmp_ino_to.o: src/tmp_ino_to.cpp \
+ .pioenvs/megaatmega2560/FrameworkArduino/Arduino.h \
+ .pioenvs/megaatmega2560/FrameworkArduino/binary.h \
+ .pioenvs/megaatmega2560/FrameworkArduino/WCharacter.h \
+ .pioenvs/megaatmega2560/FrameworkArduino/WString.h \
+ .pioenvs/megaatmega2560/FrameworkArduino/HardwareSerial.h \
+ .pioenvs/megaatmega2560/FrameworkArduino/Stream.h \
+ .pioenvs/megaatmega2560/FrameworkArduino/Print.h \
+ .pioenvs/megaatmega2560/FrameworkArduino/Printable.h \
+ .pioenvs/megaatmega2560/FrameworkArduino/USBAPI.h \
+ .pioenvs/megaatmega2560/FrameworkArduino/Arduino.h \
+ .pioenvs/megaatmega2560/FrameworkArduinoVariant/pins_arduino.h \
+ .pioenvs/megaatmega2560/LiquidCrystal/LiquidCrystal.h \
+ .pioenvs/megaatmega2560/FrameworkArduino/Print.h \
+ .pioenvs/megaatmega2560/EEPROM/EEPROM.h \
+ .pioenvs/megaatmega2560/elapsedMillis/elapsedMillis.h
